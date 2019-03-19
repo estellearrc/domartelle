254197b83410babc5af5d9ec6e23dc37dc980d86
estellearrc <estellearrc@github.com>
2019-03-19 18:17:40 +0100

    Rename files

--- client-rpi/sensors.py
+++ client-rpi/sensors.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
+import random
 
 class TemperatureSensor :
     def __init__(self,pin,stub):
@@ -7,10 +8,13 @@ class TemperatureSensor :
         self.stub = stub
     
     def RetrieveTemperature(self):
-        a=1
+        return 1
     
     def CalculateTemperature(self,data):
-        a=1
+        if self.stub:
+            return random.randint(0,30)
+        else:
+            return 1
 
 class HumiditySensor :
     def __init__(self,pin,stub):
@@ -21,7 +25,10 @@ class HumiditySensor :
         a=1
 
     def CalculateHumidityRate(self,data):
-        a=1
+        if self.stub:
+            return random.randint(0,100)
+        else:
+            return 1
 
 class MovementSensor :
     def __init__(self,pin,stub):
@@ -29,7 +36,10 @@ class MovementSensor :
         self.stub = stub
     
     def RetrieveMovement(self):
-        a=1
+        if self.stub:
+            return random.randint(0,1)
+        else:
+            return 1
 
 class LuminositySensor :
     def __init__(self,pin,stub):
@@ -37,7 +47,10 @@ class LuminositySensor :
         self.stub = stub
     
     def RetrieveLuminosity(self):
-        a=1
+        return 1
 
     def CalculateLuminosityRate(self,data):
-        a=1
+        if self.stub:
+            return random.randint(0,100)
+        else:
+            return 1
