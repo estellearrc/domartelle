6198a4594fff95a60c4f8e81c90560a6b691921b
Marieg28 <mgaillandre@ensc.fr>
2019-03-19 19:22:06 +0100

    marie test

--- client-rpi/init.py
+++ client-rpi/init.py
@@ -59,5 +59,5 @@ if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print('Killed by user')
+        print('Killed by userqkjoizjedoijzaeoid')
         sys.exit(0)
