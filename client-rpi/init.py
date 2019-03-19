90dab5c4db12eb1b44750eb6bd90645f6e586e5a
estellearrc <earricau@ensc.fr>
2019-03-19 19:19:24 +0100

    be calm

--- client-rpi/init.py
+++ client-rpi/init.py
@@ -59,5 +59,5 @@ if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print('Killed by user')
+        print('Killed by userazeregjoizehvoijefoi')
         sys.exit(0)
