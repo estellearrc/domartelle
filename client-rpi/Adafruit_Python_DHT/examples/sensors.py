9f3982f7579f2d761f013ae5018867f5174c2ff1
estellearrc <earricau@ensc.fr>
2019-04-14 17:54:48 +0200

    Update sensors.py

--- client-rpi/Adafruit_Python_DHT/examples/sensors.py
+++ client-rpi/Adafruit_Python_DHT/examples/sensors.py
@@ -70,7 +70,7 @@ class LuminositySensor :
             #fonction lisant les donnees SPI de la puce MCP3008, parmi 8 entrees, de 0 a 7
             def readadc(adcnum, clockpin, mosipin, misopin, cspin):
                 if ((adcnum > 7) or (adcnum < 0)):
-                                return -1
+                    return -1
                 GPIO.output(cspin, True)
                 GPIO.output(clockpin, False)  # start clock low
                 GPIO.output(cspin, False)    # bring CS low
@@ -78,21 +78,21 @@ class LuminositySensor :
                 commandout |= 0x18  # start bit + single-ended bit
                 commandout <<= 3    # we only need to send 5 bits here
                 for i in range(5):
-                                if (commandout & 0x80):
-                                                GPIO.output(mosipin, True)
-                                else:
-                                                GPIO.output(mosipin, False)
-                                commandout <<= 1
-                                GPIO.output(clockpin, True)
-                                GPIO.output(clockpin, False)
+                    if (commandout & 0x80):
+                        GPIO.output(mosipin, True)
+                    else:
+                        GPIO.output(mosipin, False)
+                    commandout <<= 1
+                    GPIO.output(clockpin, True)
+                    GPIO.output(clockpin, False)
                 adcout = 0
                 # read in one empty bit, one null bit and 10 ADC bits
                 for i in range(12):
-                                GPIO.output(clockpin, True)
-                                GPIO.output(clockpin, False)
-                                adcout <<= 1
-                                if (GPIO.input(misopin)):
-                                                adcout |= 0x1
+                    GPIO.output(clockpin, True)
+                    GPIO.output(clockpin, False)
+                    adcout <<= 1
+                    if (GPIO.input(misopin)):
+                        adcout |= 0x1
                 GPIO.output(cspin, True)
                 adcout /= 2    # first bit is 'null' so drop it
                 return adcout
