489d18b432d0789cb2c3120466b017f83c06dfdb
estellearrc <earricau@ensc.fr>
2019-04-24 18:53:08 +0200

    finish actuators

--- client-rpi/Adafruit_Python_DHT/examples/actuators.py
+++ client-rpi/Adafruit_Python_DHT/examples/actuators.py
@@ -31,32 +31,40 @@ class Servomotor(Actuator):
         Actuator.__init__(self, pin, room, value)
         self.type = "servo"
         self.start = 0
+        
+    def calculateAngle(self,n):
+        print(n)
+        result = float(((10 * n )/ 180)  +2)
+        return round(result,2)
 
     def instruction(self, value):
-        duty = value / 18 + 2
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.pin, GPIO.OUT)
         pwm = GPIO.PWM(self.pin, 50)
-        angle_start = calculateAngle(self.start)
-        angle_end = calculateAngle(value)
-        pwm.start(self.start)
+        angle_start = self.calculateAngle(self.start)
+        angle_end = self.calculateAngle(value)
+        print(angle_start)
+        print(angle_end)
+        pwm.start(angle_start)
         if angle_start < angle_end:
                 while angle_start < angle_end:
-                        pwm.ChangeDutyCycle(float(position))
+                        pwm.ChangeDutyCycle(float(angle_start))
                         angle_start = angle_start + 0.1
                         time.sleep(0.1)
 
         else:
                 while angle_start > angle_end:
-                        pwm.ChangeDutyCycle(float(position))
+                        pwm.ChangeDutyCycle(float(angle_start))
                         angle_start = angle_start - 0.1
                         time.sleep(0.1)
 
         self.start = value
         pwm.stop()
-        print(value)
         print("done")
 
-    def calculateAngle(value)
-    {
-        return round((2 + (10 / 180) * value, 2);}
+    
+   
+        
+        
+        
+        
